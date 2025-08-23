0feb284d065a38b2e316aa65d1652d36f65d3668
zshcole <colburnsanders@icloud.com>
2025-08-23 18:25:05 -0500

    Add back button svg to contact sheet.

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+
+export default function BackButton() {
+  return (
+    <TouchableOpacity style={styles.container}>
+        <Text><Entypo name="chevron-left" size={24} color="black" /></Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 32,
+        width: 32,
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ECF0F4'
+    }
+})
--- src/screens/ContactUsScreen.tsx
+++ src/screens/ContactUsScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import UserAvatar from '../components/UserAvatar';
+import BackButton from '../components/BackButton';
 
 export default function ContactUsScreen() {
   return (
     <View style={styles.container}>
       <UserAvatar />
+      <BackButton />
      </View>
   );
 }
