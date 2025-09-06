3f39c9749dd61be328437af319963d4a7ae66281
zshcole <colburnsanders@icloud.com>
2025-09-06 12:56:03 -0500

    add cash icon and styles

--- src/components/PaymentMethod.tsx
+++ src/components/PaymentMethod.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { CashIcon } from "./Icons";
 
 export default function PaymentMethod() {
   return (
-    <View>
-      <Text>Payment Method</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <CashIcon />
+      </View>
+      <Text style={styles.text}>Cash</Text>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: 72,
+    width: 85,
+    borderRadius: 9,
+    backgroundColor: "#F0F5FA",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: "400",
+    marginTop: 8,
+    color: "#000",
+    marginStart: 25,
+  },
+});
