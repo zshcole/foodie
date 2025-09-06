d9bea19fbcf79a5f70f3ae1d2ea03d96c9b87343
zshcole <colburnsanders@icloud.com>
2025-09-05 21:33:02 -0500

    add payment header and styles.

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import BackButton from "../components/BackButton";
 
 export default function PaymentScreen() {
   return (
-    <View>
-      <Text>Payments page</Text>
+    <View style={styles.container}>
+      <BackButton />
+      <Text style={styles.header}>Payment</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    flexDirection: "row",
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 17,
+    fontWeight: "regular",
+    color: "#181C2E",
+    marginTop: 7,
+    paddingLeft: 20,
+  },
+});
