13765143cd615c93f74dc6a78ab81f924f54ba70
zshcole <colburnsanders@icloud.com>
2025-09-15 11:28:34 -0500

    Add final styles to bank card component

--- src/components/BankCard.tsx
+++ src/components/BankCard.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { BankCardIcon } from "./Icons";
 
 export default function BankCard() {
   return (
     <View style={styles.container}>
-      <Text>Bank Card component</Text>
+      <BankCardIcon />
+      <Text style={styles.heading}>No master card added</Text>
+      <Text style={styles.subHeading}>
+        You can add a mastercard and save it for later.
+      </Text>
     </View>
   );
 }
@@ -15,5 +20,20 @@ const styles = StyleSheet.create({
     width: 340,
     borderRadius: 10,
     backgroundColor: "#F7F8F9",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#32343E",
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  subHeading: {
+    fontSize: 15,
+    fontWeight: "400",
+    width: 266,
+    textAlign: "center",
   },
 });
